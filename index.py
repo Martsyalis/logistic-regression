282a6802346bb90968712857a3a5509a39d5a39f
Maryus Martsyalis <martsyalis@gmail.com>
2020-08-07 23:21:15 -0700

    train, predict and find cm and acc score

--- index.py
+++ index.py
@@ -3,15 +3,42 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 dataset = pd.read_csv('./Social_Network_Ads.csv')
-X = dataset.iloc[:, :-1]
-y = dataset.iloc[:, -1]
+X = np.array(dataset.iloc[:, :-1])
+y = np.array(dataset.iloc[:, -1])
+
+
+
 
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state = 0)
 
+# Scale
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-print(X_test)
+# Train 
+from sklearn.linear_model import LogisticRegression
+
+classifier = LogisticRegression(random_state = 0)
+
+classifier.fit(X_train, y_train)
+
+
+
+# Predict
+y_pred = classifier.predict(X_test)
+# print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+
+
+# Compute Confusion Metrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+# Compute Accuracy Score
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(y_test, y_pred)
+
+print(acc)
